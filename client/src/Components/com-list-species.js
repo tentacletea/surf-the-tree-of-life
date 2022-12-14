1b1b7f510adc7f1e5f82e7409fc4c349416fb258
tentacletea <chen.lily@outlook.de>
2022-12-14 15:58:44 +0100

    delete redundant code

--- client/src/Components/com-list-species.js
+++ client/src/Components/com-list-species.js
@@ -2,10 +2,6 @@ import React from "react";
 import Species from "./Species"
 
 const listRender = (list) => {
-	const showDetails = () => {
-		console.log("details");
-	}
-
 	return list ? (
 		list.map((item, index) => {
 			return (
