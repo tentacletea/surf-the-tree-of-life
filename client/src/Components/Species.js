3a9bc793923e9769eca9a199b1ee933469463873
tentacletea <chen.lily@outlook.de>
2022-12-14 15:28:29 +0100

    Species

--- client/src/Components/Species.js
+++ client/src/Components/Species.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Species = ( { species } ) => {
-    const [show, setShow] = useState(false);
+// const fetchSpecies = (body, dataSetter) => {
+//     return fetch("http://localhost:8080/api/species", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(body),
+//     }).then((res) => res.json())
+// }
 
+const Species = ( { species, comment} ) => {
+
+
+    const [show, setShow] = useState(false);
+    // const [commentState, setCommentState] = useState(comment)
     const showDetails = () => {
         setShow(true)
     }
@@ -11,7 +23,8 @@ const Species = ( { species } ) => {
   return (
         show ?  
         <div>
-            <li onClick={showDetails}>Name: {species.name} Id: {species._id} Extinct: {species.extinct} Parent: {species.parent}</li>
+            <li onClick={showDetails}>Name: {species.name}  Id: {species._id} Extinct: {species.extinct.toString()} Parent: {species.parent}</li>
+            {/* <input type="text" onChange={(e) => setCommentState(e.target.value)} value={commentState}></input> */}
         </div>
         : 
         <li onClick={showDetails}>Name: {species.name}</li>
