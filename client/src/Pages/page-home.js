2506bb4a8cb205ff1a64c2657d9957cf2d5cabd2
tentacletea <chen.lily@outlook.de>
2022-12-14 13:45:07 +0100

    console data

--- client/src/Pages/page-home.js
+++ client/src/Pages/page-home.js
@@ -8,13 +8,22 @@ const fetchSpecies = (dataSetter) => {
 };
 
 const PageHome = () => {
+	const handleClick = async () => {
+		const data = await fetch("http://localhost:8080/api/species/");
+		const res = await data.json()
+		console.log(res);
+	}
+
 	const [species, setSpecies] = useState();
 	useEffect(() => {
 		fetchSpecies(setSpecies);
 	}, [species]);
+
 	return (
 		<div>
 			<h1>Welcome to Tree Of Life Project</h1>
+			<input></input>
+			<button onClick={handleClick}>Search</button>
 			<ComListSpecies species={species}></ComListSpecies>
 		</div>
 	);
